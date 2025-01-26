e7c02f7cbe517d095eedaa93b96dbad50b743dbb
John <doelpj@gmail.com>
2025-01-26 09:37:20 -0500

    login and hello test 2

--- main.py
+++ main.py
@@ -4,28 +4,34 @@
 
 import discordbot_secrets
 import discord
+from discord.ext import commands
 
 bot_login = discordbot_secrets.DISCORDKEY
 
-# This example requires the 'message_content' intent.
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
 
-import discord
+There are a number of utility commands being showcased here.'''
 
 intents = discord.Intents.default()
+intents.members = True
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', description=description, intents=intents)
 
-@client.event
+
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'Plantbot is alive as {bot.user}!')
+    print('------------------------------------')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content.startswith('!hello'):
         await message.channel.send(f'Hello {message.author}!')
 
-client.run(bot_login)
+bot.run(bot_login)
