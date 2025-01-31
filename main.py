639d1747e543fe09be3ac4eda5400e150445c802
John <doelpj@gmail.com>
2025-01-30 21:31:51 -0500

    renamed files to separate from commandnames

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import discordbot_secrets
 import discord
 from discord.ext import commands
 import random
-import moisture
+import moisture_scr
 
-#define botkey
+#define botkey, listening channel
 bot_login = discordbot_secrets.DISCORDKEY
+bot_channel = 1332750253958500414
 
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
@@ -27,18 +28,23 @@ async def on_ready():
     print(f'Plantbot is alive as {bot.user}!')
     print('------------------------------------')
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
+@bot.command(description='Says hello back to the user.')
+async def hello(ctx):
+    if ctx.author == bot.user:
         return
 
-    if message.content.startswith('!hello'):
-        await message.channel.send(f'Hello {message.author}!')
-    
-    if message.content.startswith('!roll'):
-        await message.channel.send(f'Rolled for {int(random(0,100))}.')
-    
-    if message.content.startswith('!moisture'):
-        await message.channel.send(f'Plant\'s current moisture is {moisture.grab_moisture()}.')
+    else:
+        if ctx.channel.id == bot_channel:
+            await ctx.channel.send(f'Hello {ctx.author}!')
+
+@bot.command(description='Rolls for a number between 0 - 100.')
+async def roll(ctx):
+    if ctx.channel.id == bot_channel:
+        await ctx.channel.send(f'Rolled for {int(random(0,100))}.')
+
+@bot.command(description='Captures current moisture data for the plant.')
+async def moisture_scr(ctx):
+    if ctx.channel.id == bot_channel:
+        await ctx.channel.send(f'Plant\'s current moisture is {moisture_scr.grab_moisture()}.')
 
 bot.run(bot_login)
